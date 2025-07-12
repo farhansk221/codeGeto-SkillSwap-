2d2dbd795405714a4cf018ef95fc7a08099c2de3
Singhom931 <singhom931@gmail.com>
2025-07-12 16:38:49 +0530

    conflict resolved

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -7,13 +7,8 @@ import {Link} from 'react-router-dom'
         <div className='bg-slate-800 font-bold text-white flex justify-between items-center px-3 py-5 '>
             <h2>
                 Skill Swap Platform
-            </h2>
-<<<<<<< HEAD
-            
-            <button to="/login" className='mx-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded'>
-=======
+            </h2>   
             <a href="/#/login"><button className='mx-4 bg-blue-500 hover:bg-blue-600 text-white font-semibold px-4 py-2 rounded'>
->>>>>>> 7542a32666eb78ad83c1d9769fc8d81f0128449c
                 Login
             </button></a>
         </div>
