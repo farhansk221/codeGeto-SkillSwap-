e617df5822763bf12aec8fb1c23d1cdaff23420a
farhansk221 <2024.mohd.farhan.shaikh@ves.ac.in>
2025-07-12 09:53:03 +0530

    Added two h1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1 className='bg-red-400 font-bold text-white'>SkillSwap</h1>
+            <h1 className='bg-red-400 font-bold text-white'>SkillSwap</h1>
+
     </div>
   );
 }
